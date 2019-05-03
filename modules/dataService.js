7045a138d6dd7d3540d874f2d20b3fbe4e72d2d3
PerniCZek <legat@praguesoft.cz>
2019-05-03 18:42:35 +0200

    firebase

--- modules/dataService.js
+++ modules/dataService.js
@@ -1,15 +1,32 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 const loger_1 = require("./loger");
 const sqlite3 = require('sqlite3').verbose();
+const firebase = require('firebase/app');
+const FielsValue = require('firebase-admin').firestore.FieldValue;
+const admin = require('firebase-admin');
+const serviceAccount = require('../serviceAccount.json');
 class DataService {
     constructor() {
         this.loger = new loger_1.Loger();
-        this.db = new sqlite3.Database('https://karel-prgsft.github.io/dcBotCodeBar/db/codeBar.db', sqlite3.OPEN_READONLY, (err) => {
+
+        /*
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+        });
+        const fb = admin.firestore();
+        fb.collection('test').doc('123').set({
+            'id': '123',
+            'ebe': 'test',
+        });
+        */
+
+        this.db = new sqlite3.Database('./db/codeBar.db', sqlite3.OPEN_READONLY, (err) => {
             if (err) {
                 this.loger.log(err.message, 'error');
-            }
-            else {
+            } else {
                 this.loger.log('Připojen k db/codeBar.db', 'database');
             }
         });
--- src/modules/dataService.ts
+++ src/modules/dataService.ts
@@ -1,15 +1,35 @@
 ///<reference path="../../node_modules/@types/sqlite3/index.d.ts"/>
+///<reference path="../../node_modules/firebase-admin/lib/index.d.ts"/>
+///<reference path="../../node_modules/firebase/index.d.ts"/>
 
 import { Loger } from './loger';
 import { sqlite3, Database } from 'sqlite3';
+import { Firestore } from '@google-cloud/firestore';
+
 const sqlite3: sqlite3 = require('sqlite3').verbose();
+const firebase = require('firebase/app');
+const FielsValue = require('firebase-admin').firestore.FieldValue;
+const admin = require('firebase-admin');
+const serviceAccount = require('../serviceAccount.json');
 
 export class DataService {
   private loger = new Loger();
   public db: Database;
 
   constructor() {
-    this.db = new sqlite3.Database('https://karel-prgsft.github.io/dcBotCodeBar/db/codeBar.db', sqlite3.OPEN_READONLY, (err) => {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount)
+    });
+
+
+    const fb: Firestore = admin.firestore();
+
+    fb.collection('test').doc('1234').set({
+      'id': '1234',
+      'ebe': 'test2',
+    });
+
+    this.db = new sqlite3.Database('./db/codeBar.db', sqlite3.OPEN_READONLY, (err) => {
       if (err) {
         this.loger.log(err.message, 'error');
       } else {
